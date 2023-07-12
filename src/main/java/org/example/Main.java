a1ed65e050ff884a15d8c7708515ac56cae055a4
Buket Yurt <buketyurt01@gmail.com>
2023-07-13 00:33:16 +0300

    make small changes * "TL added" * Scanners places changed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,12 +11,14 @@ public class Main {
 
         System.out.println("Mesafeyi km türünden giriniz : ");
         double Mesafe = scanner.nextDouble();
-
+        System.out.println("Yaşınızı giriniz : ");
+        double Age = scanner.nextDouble();
+        System.out.println("Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ): ");
+        int TripType = scanner.nextInt();
         if (Mesafe > 0) { //control the input is positive
             NormalPrice = Mesafe*0.10;  //calculate normal price
             //System.out.println(NormalPrice);
-            System.out.println("Yaşınızı giriniz : ");
-            double Age = scanner.nextDouble();
+
             if(Age > 0) { //control the input is positive
                 if (Age < 12) {//check age and make appropriate discount for this person
                     AgeDiscount = NormalPrice * 0.5;
@@ -28,17 +30,16 @@ public class Main {
                 //System.out.println(AgeDiscount);
                 DiscountedPrice = NormalPrice -AgeDiscount;
                 //System.out.println(DiscountedPrice);
-                System.out.println("Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ): ");
-                int TripType = scanner.nextInt();
+
                     if(TripType>0){ //control the input is positive
                         if(TripType == 1){ //check trip type and make appropriate calculation fo ticket
                             Total= DiscountedPrice;
-                            System.out.println("Toplam Tutar : " + Total);
+                            System.out.println("Toplam Tutar : " + Total + " TL");
                         }
                         else if(TripType == 2){
                             TypeDiscount= DiscountedPrice*0.2;
                             Total = (DiscountedPrice -TypeDiscount)*2;
-                            System.out.println("Toplam Tutar : " + Total);
+                            System.out.println("Toplam Tutar : " + Total + " TL");
                         }
                     }else{
                         System.out.println("Hatalı Veri Girdiniz !");
